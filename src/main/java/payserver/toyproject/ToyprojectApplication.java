de90aeb37e422db2bcf3ea5182c0195e409eae04
이효곤 <LeeHyogon>
2022-09-03 03:19:58 +0900

    paymentController 구현

--- src/main/java/payserver/toyproject/ToyprojectApplication.java
+++ src/main/java/payserver/toyproject/ToyprojectApplication.java
@@ -2,10 +2,16 @@ package payserver.toyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToyprojectApplication {
+public class ToyprojectApplication  extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ToyprojectApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ToyprojectApplication.class, args);
 	}
